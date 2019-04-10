2bffbaddd5a7ed2bfbe7b59bb0b5d1d04a070a68
nobikko <nobikko@users.noreply.github.com>
2019-04-11 03:24:25 +0900

    Enable LIN_ADVANCE again
    Enable LIN_ADVANCE, define MINIMUM_STEPPER_PULSE 1 and use SpreadCycle for E0

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1038,7 +1038,7 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-//#define LIN_ADVANCE
+#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0	  // Unit: mm compression per 1mm/s extruder speed
@@ -1131,7 +1131,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MINIMUM_STEPPER_PULSE 2
+#define MINIMUM_STEPPER_PULSE 1
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
@@ -1584,7 +1584,7 @@
    */
   #define STEALTHCHOP_XY
   #define STEALTHCHOP_Z
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
